a32bc289e6448efbe229ed0021d638b2460459a0
wellerson pinheiros <wellersonpinheiros@outlook.com>
2024-12-02 16:32:30 -0300

    Primeira aula de JS com node

---
+++ aula_01/testes.js
@@ -0,0 +1,19 @@
+const leia = require('readline-sync');
+let n1; 
+let n2;
+
+n1 = leia.questionInt("digite o primeiro numero: ")
+n2 = leia.questionInt("digite o segundo numero: ")
+
+
+console.log('a váriavel n1 possui o valor:'  + n1)
+console.log('A variavel n2 possui o valor:' +  n2)
+
+console.log('A variavel n2 possui o valor:' +  n2 + n1)
+console.log('A variavel n2 possui o valor:' +  n2 - n1)
+console.log('A variavel n2 possui o valor:' +  n2 / n1)
+console.log('A variavel n2 possui o valor:' +  n2 * n1)
+
+
+
+105
